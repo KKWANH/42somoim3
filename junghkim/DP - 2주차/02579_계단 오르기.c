c348892d79d888d9960f241a3f4bac41344b6232
jj150618 <gimhun0@gmail.com>
2020-10-24 16:13:51 +0900

    DP - 2주차

--- junghkim/DP - 2주차/02579_계단 오르기.c
+++ junghkim/DP - 2주차/02579_계단 오르기.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-int max(int a, int b, int c)
+int max(int a, int b)
 {
     int max;
 
     max = a;
     if (max < b)
         max = b;
-    if (max < c)
-        max = c;
     return (max);
 }
 
 int main(void)
 {
-    int array[2][10001];
+    int array[2][301];
     int i, j;
 
     scanf("%d", &i);
@@ -25,6 +23,6 @@ int main(void)
     array[1][2] = array[0][1] + array[0][2];
     j = 2;
     while (++j <= i)
-        array[1][j] = max(array[1][j - 2] + array[0][j], array[1][j - 3] + array[0][j - 1] + array[0][j], array[1][j - 1]);
+        array[1][j] = max(array[1][j - 2] + array[0][j], array[1][j - 3] + array[0][j - 1] + array[0][j]);
     printf("%d", array[1][i]);
 }
